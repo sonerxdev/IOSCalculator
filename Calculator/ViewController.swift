766e659b5c46b5c2485f928aa91860b5bc8d8d3b
sonerxdev <sonerkaraevli@gmail.com>
2022-03-27 15:41:08 +0300

    Completed.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var result: Int!
 
+    @IBOutlet weak var firstNumberTxtField: UITextField!
+    
+    @IBOutlet weak var secondNumberTxtField: UITextField!
+    
+    @IBOutlet weak var resultTxt: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plusFunc(_ sender: Any) {
+        
+        if let firstNumber = Int(firstNumberTxtField.text!){
+            if let secondNumber = Int(secondNumberTxtField.text!){
+                result = firstNumber + secondNumber
+                resultTxt.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func minusFunc(_ sender: Any) {
+        if let firstNumber = Int(firstNumberTxtField.text!){
+            if let secondNumber = Int(secondNumberTxtField.text!){
+                result = firstNumber - secondNumber
+                resultTxt.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func multiplicationFunc(_ sender: Any) {
+        if let firstNumber = Int(firstNumberTxtField.text!){
+            if let secondNumber = Int(secondNumberTxtField.text!){
+                result = firstNumber * secondNumber
+                resultTxt.text = String(result)
+            }
+        }
+    }
+    @IBAction func dividingFunc(_ sender: Any) {
+        if let firstNumber = Int(firstNumberTxtField.text!){
+            if let secondNumber = Int(secondNumberTxtField.text!){
+                result = firstNumber / secondNumber
+                resultTxt.text = String(result)
+            }
+        }
+    }
 }
 
